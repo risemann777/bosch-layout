13ca0d3f034d8c7060acffe40feb896ae5a766e4
SemenOn <risemann777@gmail.com>
2024-03-26 17:57:03 +0300

    wide slider, news slider, server + style fixes

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -15,6 +15,34 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
+  new Swiper('#wideSlider', {
+    modules: [Navigation, Pagination],
+    autoHeight: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  });
+
+  new Swiper('#newsSlider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      992: {
+        spaceBetween: 32
+      }
+    }
+  });
+
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
